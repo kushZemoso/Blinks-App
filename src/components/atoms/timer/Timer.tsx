f62d246fce31be7600154dba4aa5d000412af339
kushZemoso <kushagra.jain@zemosolabs.com>
2022-03-01 23:36:23 +0530

    Revert "Blinklist App"
    This reverts commit 9b8fbcbd8c804edd43a5bb68815aa34247566932.

--- src/components/atoms/timer/Timer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-const Timer = () => {
-  return (<AccessTimeIcon data-testid="Timer" ></AccessTimeIcon>);
-};
-
-export default Timer;
--- src/components/molecules/footer/footer-navlist/FooterNavListTwo.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {screen,cleanup, render} from "@testing-library/react";
-import "@testing-library/jest-dom";
-import FooterNavListTwo from "./FooterNavListTwo";
-afterEach(cleanup);
-test("Checking Footer Nav List Two Rendering",()=>{
-  render(<FooterNavListTwo />);
-  expect(screen.getByTestId("FooterNavList2")).toBeInTheDocument();
-});
